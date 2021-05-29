7b1d6de026a07c438e111359d78358c33c2c2129
Anjalicoding <66794052+Anjalicoding@users.noreply.github.com>
2021-05-29 17:40:37 +0530

    Add favourites tab

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,11 +8,35 @@
 
 //Action types
 export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_FAVOURITE = "ADD_FAVOURITE";
+export const REMOVE_FAVOURITE = "REMOVE_FAVOURITE";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
 
 //Action creators
 export function addMovies(movies) {
   return {
     type: ADD_MOVIES,
     movies
+  };
+}
+
+export function addFavourite(movie) {
+  return {
+    type: ADD_FAVOURITE,
+    movie
+  };
+}
+
+export function removeUnFavourite(movie) {
+  return {
+    type: REMOVE_FAVOURITE,
+    movie
+  };
+}
+
+export function setShowFavourites(val) {
+  return {
+    type: SET_SHOW_FAVOURITES,
+    val
   };
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,48 @@
-import { ADD_MOVIES } from "../actions";
+import {
+  ADD_MOVIES,
+  ADD_FAVOURITE,
+  REMOVE_FAVOURITE,
+  SET_SHOW_FAVOURITES
+} from "../actions";
 
 const InitialMovieState = {
   list: [],
-  favourites: []
+  favourites: [],
+  showFavourites: false
 };
 export default function movies(state = InitialMovieState, action) {
-  if (action.type === ADD_MOVIES) {
-    return {
-      ...state,
-      list: action.movies
-    };
+  // if (action.type === ADD_MOVIES) {
+  //   return {
+  //     ...state,
+  //     list: action.movies
+  //   };
+  // }
+  // return state;
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        list: action.movies
+      };
+    case ADD_FAVOURITE:
+      return {
+        ...state,
+        favourites: [action.movie, ...state.favourites]
+      };
+    case REMOVE_FAVOURITE:
+      const filteresdArray = state.favourites.filter(
+        (movie) => movie.Title !== action.movie.Title
+      );
+      return {
+        ...state,
+        favourites: filteresdArray
+      };
+    case SET_SHOW_FAVOURITES:
+      return {
+        ...state,
+        showFavourites: action.val
+      };
+    default:
+      return state;
   }
-  return state;
 }
